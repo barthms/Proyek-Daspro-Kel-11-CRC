1523d62e3302fda7e506be198bee749637b358d3
barthms <andikabartolomeus677@gmail.com>
2024-12-01 15:38:31 +0700

    Menambahkan fungsi FILE untuk membaca dan menulis pada file .txt

--- CRC.c
+++ CRC.c
@@ -1,53 +1,94 @@
 #include <stdio.h>
 #include<string.h>
 
-int binner(int angka, int result[]){
+int binner(int nim, int result[]){
     int i;
-    for (i = 0; angka > 0; i++){
-        result[i] = angka % 2;
-        angka = angka / 2;
+    for (i = 0; nim > 0; i++){
+        result[i] = nim % 2;
+        nim = nim / 2;
     }
     
     return i;
 }
 
-void polynominal (int result[], int panjang){
+int polynominal (int result[], int panjang, FILE *keluaran){
     int kondisi = 1;
     for (int i = panjang - 1; i >= 0; i--){
         if (result[i] == 1){
             if (!kondisi){
-                printf("+");
+                fprintf(keluaran, "+");
             }
             if (i == 0){
-                printf("1");
+                fprintf(keluaran, "1");
             }else if(i == 1){
-                printf("x");
+                fprintf(keluaran, "x");
             }else{
-                printf("x^%d", i);
+                fprintf(keluaran ,"x^%d", i);
             }
             kondisi = 0;
         }
         
     }
+    return kondisi;
 }
 
-int main(){
-    int angka, result[32];
-    int panjang;
-
-    printf("Masukan angka Anda :");
-    scanf("%d", &angka);
-
-    panjang = binner(angka, result);
-
-    printf("Biner dari %d adalah :", angka);
+int primitif(int result[], int panjang, int resultPrimitif[]){
+    int n = 0;
     for (int i = panjang - 1; i >= 0; i--){
-        printf("%d", result[i]);
+        if (result[i] == 1){
+            n = i;
+            break;
+        }
     }
-    printf("\n");
 
-    printf("Polynominal dari %d adalah :", angka);
-    polynominal(result, panjang);
+    for (int i = 0; i < panjang; i++){
+        if (i == n){
+            resultPrimitif[i] = 0;
+        }else{
+            resultPrimitif[i] = result[i];
+        }
+        
+    }
+    return panjang;
+}
+
+
+
+int main(){
+    int nim, result[32]; 
+    int panjang, panjangPrimitif;
+    int resultPrimitif[32];
+    FILE *inputan, *outputan;
+
+    inputan = fopen("input.txt", "r");
+    if (inputan == NULL){
+        printf("Gagal membuka file input.txt\n");
+        return 1;
+    }
+    
+    outputan = fopen("output.txt", "w");
+
+    fprintf(outputan, "*********PROGRAM CRC*********\n");
+    fprintf(outputan, "-------------------------------\n");
+
+    while(fscanf(inputan, "%d", &nim) != EOF){
+        fprintf(outputan, "NIM: %d\n", nim);
+
+        // Mengkonversi nim ke biner
+        panjang = binner(nim, result);
+        fprintf(outputan, "Binary: ");
+        for (int i = panjang - 1; i >= 0; i--){
+            fprintf(outputan, "%d", result[i]);
+        }
+        fprintf(outputan, "\n");
+
+        // Mengkonversi biner ke polynomial 
+        fprintf(outputan, "Polynomial :");
+        polynominal(result, panjang, outputan);
+        fprintf(outputan, "\n-------------------------------\n");
+
+    }
+    
 
     return 0;
 }
